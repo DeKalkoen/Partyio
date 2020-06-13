05594057be505256ae1c0dd3fb1cd55276e51dec
DeKalkoen <47044950+DeKalkoen@users.noreply.github.com>
2020-06-13 14:29:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function initiate(){
+	if (window.addEventListener) {
+		window.addEventListener("load", autorun, false);
+	}
+	else if (window.attachEvent){
+		window.attachEvent("onload", autorun);
+	}
+	else {
+		window.onload = autorun;
+	}
+}
+
+function autorun(){
+	
+}
